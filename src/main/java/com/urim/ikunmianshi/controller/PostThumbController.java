9c19504c79f1cd023eca8b859bd5c784f06700a9
L1n. <zhl20040514@outlook.com>
2025-12-03 16:11:11 +0800

    后端项目初始化

---
+++ src/main/java/com/urim/ikunmianshi/controller/PostThumbController.java
@@ -0,0 +1,55 @@
+package com.urim.ikunmianshi.controller;
+
+import com.urim.ikunmianshi.common.BaseResponse;
+import com.urim.ikunmianshi.common.ErrorCode;
+import com.urim.ikunmianshi.common.ResultUtils;
+import com.urim.ikunmianshi.exception.BusinessException;
+import com.urim.ikunmianshi.model.dto.postthumb.PostThumbAddRequest;
+import com.urim.ikunmianshi.model.entity.User;
+import com.urim.ikunmianshi.service.PostThumbService;
+import com.urim.ikunmianshi.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ *
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/test/java/com/urim/ikunmianshi/iKunMianShiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.urim.ikunmianshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ */
+@SpringBootTest
+class iKunMianShiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
